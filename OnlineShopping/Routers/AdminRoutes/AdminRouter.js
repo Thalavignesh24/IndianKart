4716ce3ec2a3719a592e54930323b315d4669acf
Vignesh.K <vigneshk24082000@gmail.com>
2023-03-14 19:20:57 +0530

    Indiankart

--- OnlineShopping/Routers/AdminRoutes/AdminRouter.js
+++ OnlineShopping/Routers/AdminRoutes/AdminRouter.js
@@ -7,9 +7,17 @@ router.get('/CustomerDetailsDownload', AdminController.CustomerExcel);
 
 router.post('/AddTemplates', AdminController.addTemplates);
 
-router.post('/AddProducts', validator.productValidatio, AdminController.addProducts);
+router.post('/AddProducts', validator.productValidation, AdminController.addProducts);
 
 router.delete('/DeleteProducts/:ProductId', AdminController.deleteProducts);
+
+router.get('/ListOfProducts', AdminController.listOfProducts);
+
+router.get('/ActiveProducts', AdminController.activeProducts);
+
+router.get('/StatusChange/:ProductId', AdminController.statusChange);
+
+router.put('/ProductUpdate', validator.productUpdateValidation, AdminController.productUpdate);
 
 module.exports = router;
 
--- OnlineShopping/Validation/AdminValidation.js
+++ OnlineShopping/Validation/AdminValidation.js
@@ -1,10 +1,20 @@
 const { validator, handleRequest } = require('./CommonValidation');
 
 function validation() {
-    this.productValidatio = [
+    this.productValidation = [
         validator.checkName('ProductName', 'Product name'),
         validator.checkQty('ProductQuantity', 'Product quantity'),
         validator.checkAmt('ProductAmount', 'Product amount'),
+        validator.checkDescription('ProductDescription', 'Product description'),
+        handleRequest
+    ]
+
+    this.productUpdateValidation = [
+        validator.checkId('ProductId', 'product id'),
+        validator.checkName('ProductName', 'Product name'),
+        validator.checkQty('ProductQuantity', 'Product quantity'),
+        validator.checkAmt('ProductAmount', 'Product amount'),
+        validator.checkDescription('ProductDescription', 'Product description'),
         handleRequest
     ]
 }
