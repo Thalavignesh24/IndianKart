727a91e8f3e1513320457135e523809ec9830031
Vignesh.K <vigneshk24082000@gmail.com>
2023-03-24 17:25:42 +0530

    IndianKart

--- OnlineShopping/server.js
+++ OnlineShopping/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 var fileupload = require("express-fileupload");
 const device = require('express-device');
 var useragent = require('express-useragent');
+const ratelimiter = require('express-rate-limit');
 
 app.use(useragent.express());
 app.use(bodyParser.json());
@@ -13,6 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileupload({ useTempFiles: true }));
 app.use(express.static(path.join(__dirname, 'Public')));
 app.use(device.capture());
+
+const limiter = ratelimiter({
+    windowMs: 1 * 60 * 1000,
+    max: 2, standardHeader: true,
+    message: "Too many requests hitted, please try again later."
+});
+app.use(limiter);
 
 app.set('views', path.join(__dirname, 'Frontend'));
 app.set('view engine', 'ejs');
